ec239090b4b98386b876e054b8c74e35e40b284e
Trung Hiếu Trương <kimthu86zz@gmail.com>
2024-08-10 09:10:16 +0700

    Add project files.

---
+++ LCHB_BookingsMng/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LCHB_BookingsMng.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
