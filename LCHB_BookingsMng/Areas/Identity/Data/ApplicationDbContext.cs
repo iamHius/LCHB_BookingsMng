82be478d1811e1c796ede47dfc5c1f628c1692f5
QuangCong09 <2100010440@nttu.edu.vn>
2024-08-10 11:07:18 +0700

    Scaffold Identity for Login,Logout, Register

---
+++ LCHB_BookingsMng/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using LCHB_BookingsMng.Areas.Identity.Data;
+using LCHB_BookingsMng.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LCHB_BookingsMng.Areas.Identity.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<RoomType> RoomTypes { get; set; }
+    public DbSet<RoomStatus> RoomStatuses { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<RoomService> RoomServices { get; set; }
+    public DbSet<BookingRoom> Bookings { get; set; }
+    public DbSet<BookingService> BookingsService { get; set; }
+    public DbSet<Bill> Bills { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(200);
+        builder.Property(u => u.LastName).HasMaxLength(200);
+    }
+}
--- LCHB_BookingsMng/Program.cs
+++ LCHB_BookingsMng/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using LCHB_BookingsMng.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -23,5 +31,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
